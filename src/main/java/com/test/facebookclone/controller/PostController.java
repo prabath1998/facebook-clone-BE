b59c5dd92cd6f10cd76ff6eba0ac3916c93999fe
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-03 18:01:23 +0530

    change project structure

--- src/main/java/com/test/facebookclone/controller/PostController.java
+++ src/main/java/com/test/facebookclone/controller/PostController.java
@@ -1,9 +1,15 @@
 package com.test.facebookclone.controller;
 
+import com.test.facebookclone.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
+    @Autowired
+    private PostService postService;
 }
--- src/main/java/com/test/facebookclone/service/PostServiceImpl.java
+++ src/main/java/com/test/facebookclone/service/PostServiceImpl.java
@@ -1,7 +1,11 @@
 package com.test.facebookclone.service;
 
+import com.test.facebookclone.repository.PostEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService{
+    @Autowired
+    private PostEntityRepository repository;
 }
